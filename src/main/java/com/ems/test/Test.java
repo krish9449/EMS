9c16a30aea64c7e89b442be361b3de0947c043ba
krish9449 <krishnareddy9449@gmail.com>
2019-01-17 22:11:23 +0530

    added add() into test class

--- src/main/java/com/ems/test/Test.java
+++ src/main/java/com/ems/test/Test.java
@@ -2,9 +2,13 @@ package com.ems.test;
 
 public class Test {
 	
-	
+	public static int add(int a, int b) {
+		return a+b;
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
+		
+		System.out.println(add(10, 20));
 	}
 }
